949176e8cb84454331b3788fdfe9612436a01dfb
Ashenafi-Tesfaye <65870089+Ashenafi-Tesfaye@users.noreply.github.com>
2024-05-31 21:14:29 -0400

    initial commit for ITEC 630 9042 Concert Ticket Sales Application System Analysis and Design individual project

---
+++ src/main/java/ITEC6309042/com/ConcertTicketSalesApplication/ConcertTicketSalesApplication.java
@@ -0,0 +1,13 @@
+package ITEC6309042.com.ConcertTicketSalesApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertTicketSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertTicketSalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ITEC6309042/com/ConcertTicketSalesApplication/ConcertTicketSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package ITEC6309042.com.ConcertTicketSalesApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertTicketSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
